4fd58bdd945339857c87dbb2631072cf3e5140b5
Damian Dorosz <DoroszDamian@protonmail.com>
2023-08-07 01:05:52 +0200

    Reformat

--- DioBrandoBot.App/Program.cs
+++ DioBrandoBot.App/Program.cs
@@ -6,49 +6,48 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace DioBrandoBot.App
+namespace DioBrandoBot.App;
+
+public static class Program
 {
-    public static class Program
+    private static async Task Main(string[] args)
     {
-        private static async Task Main(string[] args)
+        var baseDirectory = Directory.GetParent(AppContext.BaseDirectory) ?? throw new InvalidOperationException("Couldn't get the base directory or its parent!");
+        // // TODO: get it in a more sensible way
+        var baseDirectoryPath = baseDirectory.Parent?.Parent?.Parent;
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(baseDirectoryPath!.FullName)
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+        var services = new ServiceCollection();
+        ConfigureServices(services, configuration);
+
+        await using var serviceProvider = services.BuildServiceProvider();
+        var bot = ActivatorUtilities.CreateInstance<BotController>(serviceProvider);
+
+        await bot.Run();
+    }
+
+    private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        var discordClientConfiguration = new DiscordConfiguration
         {
-            var baseDirectory = Directory.GetParent(AppContext.BaseDirectory) ?? throw new InvalidOperationException("Couldn't get the base directory or its parent!");
-            // TODO: get it in a more sensible way
-            var baseDirectoryPath = baseDirectory.Parent?.Parent?.Parent;
-            IConfiguration configuration = new ConfigurationBuilder()
-                .SetBasePath(baseDirectoryPath!.FullName)
-                .AddJsonFile("appsettings.json", false, true)
-                .Build();
+            Token = configuration.GetSection("DISCORD_BOT_TOKEN").Value ?? throw new InvalidOperationException("Couldn't get the Discord bot token!"),
+            TokenType = TokenType.Bot,
+            Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
+            MinimumLogLevel = LogLevel.Information
+        };
+        var discordClient = new DiscordClient(discordClientConfiguration);
+        services.AddScoped<DiscordClient>(x => discordClient);
 
-            var services = new ServiceCollection();
-            ConfigureServices(services, configuration);
-
-            await using var serviceProvider = services.BuildServiceProvider();
-            var bot = ActivatorUtilities.CreateInstance<BotController>(serviceProvider);
-
-            await bot.Run();
-        }
-
-        private static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        var slashCommands = discordClient.UseSlashCommands(new SlashCommandsConfiguration
         {
-            var discordClientConfiguration = new DiscordConfiguration
-            {
-                Token = configuration.GetSection("DISCORD_BOT_TOKEN").Value ?? throw new InvalidOperationException("Couldn't get the Discord bot token!"),
-                TokenType = TokenType.Bot,
-                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents,
-                MinimumLogLevel = LogLevel.Information,
-            };
-            var discordClient = new DiscordClient(discordClientConfiguration);
-            services.AddScoped<DiscordClient>(x => discordClient);
+            Services = new ServiceCollection().AddScoped<IAiService, AiService>().BuildServiceProvider()
+        });
+        slashCommands.RegisterCommands<SlashCommands>();
 
-            var slashCommands = discordClient.UseSlashCommands(new SlashCommandsConfiguration
-            {
-                Services = new ServiceCollection().AddScoped<IAiService, AiService>().BuildServiceProvider()
-            });
-            slashCommands.RegisterCommands<SlashCommands>();
-
-            services.AddScoped<IAiService, AiService>();
-            services.AddLogging(x => x.AddConsole()).AddTransient<BotController>();
-        }
+        services.AddScoped<IAiService, AiService>();
+        services.AddLogging(x => x.AddConsole()).AddTransient<BotController>();
     }
 }
